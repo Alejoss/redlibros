7f24194ec983ff4000254e6001989eba2c96fcaa
Alejoss <alejoveintimilla@gmail.com>
2015-05-20 17:16:38 -0500

    settings sendgrid

--- redlibros/settings.py
+++ redlibros/settings.py
@@ -140,9 +140,9 @@ if HEROKU:
     SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ["SOCIAL_AUTH_GOOGLE_KEY"]
 
 
-# Sendgrid
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'sendgrid_username'
-EMAIL_HOST_PASSWORD = 'sendgrid_password'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+    # Sendgrid
+    EMAIL_HOST = 'smtp.sendgrid.net'
+    EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+    EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
