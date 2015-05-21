55c242a270fc9d6f4dc1876f323eda68d5c29532
Alejoss <alejoveintimilla@gmail.com>
2015-05-20 19:04:34 -0500

    remover field nickname

--- perfiles/models.py
+++ perfiles/models.py
@@ -5,8 +5,7 @@ from cities_light.models import City
 
 
 class Perfil(models.Model):
-	usuario = models.OneToOneField(User, null=True)
-	nickname = models.CharField(max_length=75, unique=True, blank=True)
+	usuario = models.OneToOneField(User, null=True)	
 	imagen_perfil = models.CharField(max_length=255, blank=True)
 	descripcion = models.CharField(max_length=250, blank=True)
 
